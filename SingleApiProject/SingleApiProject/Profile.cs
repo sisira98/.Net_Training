0bb8726efcb37c1cbb4e2ee25f5419002351c794
Sisira J S <sisira.s@edstem.com>
2023-06-30 15:10:41 +0530

    initial commit

---
+++ SingleApiProject/SingleApiProject/Profile.cs
@@ -0,0 +1,12 @@
+namespace SingleApiProject;
+
+public class Profile
+{
+    public string? Name { get; set; }
+
+    public int Age { get; set; }
+
+    public string? Gender { get; set; } 
+
+    public string? Place { get; set; }
+}
